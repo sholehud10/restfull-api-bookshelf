194cd4e48dc020285c601cbdf40df6a7a9ac1652
sholehud10 <sholehud10@gmail.com>
2022-11-05 19:34:31 +0700

    create function addBookHandler in handler.js and some require

---
+++ src/handler.js
@@ -0,0 +1,79 @@
+const { nanoid } = require('nanoid');
+
+const Books = require('./books');
+
+const addBookHandler = (request, h) => {
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+
+  const id = nanoid(16);
+
+  const insertAt = new Date().toISOString();
+  const updateAt = insertAt;
+
+  const addNewBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+    insertAt,
+    updateAt,
+  };
+
+  Books.push(addNewBook);
+
+  const isSuccess = Books.filter((book) => book.id === id).length > 0;
+
+  if (name === undefined) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+    });
+    response.code(400);
+    return response;
+  }
+
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message:
+        'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+    response.code(400);
+    return response;
+  }
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal ditambahkan',
+  });
+  response.code(500);
+  return response;
+};
+
+module.exports = { addBookHandler };
