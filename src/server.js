bcbdcb210c1b331c744a07d3df6f657a90bb5bba
sholehud10 <sholehud10@gmail.com>
2022-11-05 17:59:06 +0700

    enable all cors in route

--- src/server.js
+++ src/server.js
@@ -4,6 +4,11 @@ const init = async () => {
   const Server = Hapi.server({
     port: 5000,
     host: 'localhost',
+    routes: {
+      cors: {
+        origin: '*',
+      },
+    },
   });
 
   await Server.start();
