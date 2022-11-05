910e4947952d94ca6d51402b8322b589b12a24b9
sholehud10 <sholehud10@gmail.com>
2022-11-05 23:02:12 +0700

    adding delete handler function

--- src/handler.js
+++ src/handler.js
@@ -110,7 +110,7 @@ const getDetailBookHandler = (request, h) => {
   return response;
 };
 
-const editBookHandler = (request, h) => {
+const updateBookHandler = (request, h) => {
   const { id } = request.params;
 
   const {
@@ -179,9 +179,33 @@ const editBookHandler = (request, h) => {
   return response;
 };
 
+const deleteBookHandler = (request, h) => {
+  const { id } = request.params;
+
+  const index = books.findIndex((b) => b.id === id);
+
+  if (index !== -1) {
+    books.splice(index, 1);
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil dihapus',
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal dihapus. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
 module.exports = {
   getAllBooksHandler,
   getDetailBookHandler,
   addBookHandler,
-  editBookHandler,
+  updateBookHandler,
+  deleteBookHandler,
 };
